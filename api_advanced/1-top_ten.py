e0f9be189f94cec156958df19ad12e12bd56116e
DLOADIN <m.david@alustudent.com>
2024-11-12 04:02:35 -0800

    another

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""DOCS"""
-import requests
 
-
+"""
+Displays the titles of 10 hot posts listed for a subreddit
+"""
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
-
+    """Print the titles of the first 10 hot posts listed for a given subreddit."""
+    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    
+    # Prevent following redirects
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        try:
+            data = response.json().get('data', {})
+            posts = data.get('children', [])
+            if posts:
+                for post in posts[:10]:
+                    print(post['data'].get('title'))
+            else:
+                print(None)
+        except (KeyError, ValueError):
+            # If there's an issue with accessing JSON or it doesn't have expected structure
+            print(None)
     else:
         print(None)
